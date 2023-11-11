e0a030fc9a05346f9aad49073a4a84548e280010
durimpacarizi <150191578+durimpacarizi@users.noreply.github.com>
2023-11-11 02:21:12 +0100

    HomePage 100% done

--- dummyData.js
+++ dummyData.js
@@ -1,66 +1,123 @@
-import logo1 from "./images/logo1.png";
-import logo2 from "./images/logo2.png";
-import logo3 from "./images/logo3.png";
-import logo4 from "./images/logo4.png";
-import project1 from "./images/project-1.jpeg";
-import project2 from "./images/project-2.jpeg";
-import project3 from "./images/project-3.jpeg";
+const ProjectsListData = {
+  projects: [
+    {
+      id: 1,
+      ProjectName: "Field House",
+      projectImages: [
+        "https://images.adsttc.com/media/images/653c/110c/08e4/4471/0253/5d10/slideshow/field-house-birdseye_1.jpg?1698435377",
+        "https://images.adsttc.com/media/images/653c/110c/08e4/4471/0253/5d10/slideshow/field-house-birdseye_1.jpg?1698435377",
+        "https://images.adsttc.com/media/images/653c/110e/f96c/7632/7f25/264a/slideshow/field-house-birdseye_6.jpg?1698435382",
+      ],
+      url: "#",
+    },
+    {
+      id: 2,
+      ProjectName: "House 1710",
+      projectImages: [
+        "https://images.adsttc.com/media/images/6527/47b5/64e5/0c1f/072c/eacb/slideshow/casa-1710-jle-arquitectos_2.jpg?1697073119",
+        "https://images.adsttc.com/media/images/6527/47b4/0e26/f37b/eee3/9ea9/slideshow/casa-1710-jle-arquitectos_4.jpg?1697073097",
+        "https://images.adsttc.com/media/images/6527/47b9/0e26/f37b/eee3/9ead/slideshow/casa-1710-jle-arquitectos_3.jpg?1697073168",
+      ],
+      url: "#",
+    },
+    {
+      id: 3,
+      ProjectName: "Dimmick Drive Houses",
+      projectImages: [
+        "https://images.adsttc.com/media/images/64ed/e04e/bbf5/6326/8b15/d425/slideshow/dimmick-drive-houses-freelandbuck_13.jpg?1693311072",
+        "https://images.adsttc.com/media/images/64ed/e02c/5286/7b47/fb7a/581d/slideshow/dimmick-drive-houses-freelandbuck_2.jpg?1693311044",
+        "https://images.adsttc.com/media/images/64ed/e03a/5286/7b47/fb7a/581f/slideshow/dimmick-drive-houses-freelandbuck_1.jpg?1693311051",
+      ],
+      url: "#",
+    },
+    {
+      id: 4,
+      ProjectName: "La Perouse House",
+      projectImages: [
+        "https://images.adsttc.com/media/images/64ca/bf82/4b88/3f34/2c35/c0ea/slideshow/casa-la-perouse-elton-leniz_2.jpg?1691008942",
+        "https://images.adsttc.com/media/images/64ca/bf80/4b88/3f34/2c35/c0e9/slideshow/casa-la-perouse-elton-leniz_1.jpg?1691008942",
+        "https://images.adsttc.com/media/images/64ca/bf84/4b88/3f39/2185/7eb9/slideshow/casa-la-perouse-elton-leniz_6.jpg?1691008953",
+      ],
+      url: "#",
+    },
+    {
+      id: 5,
+      ProjectName: "2C House",
+      projectImages: [
+        "https://images.adsttc.com/media/images/6528/d469/0e26/f37a/f230/ca03/slideshow/casa-2c-baquio-arquitectura_5.jpg?1697174669",
+        "https://images.adsttc.com/media/images/6528/d462/64e5/0c4c/f750/b849/slideshow/casa-2c-baquio-arquitectura_4.jpg?1697174655",
+        "https://images.adsttc.com/media/images/6528/d476/64e5/0c4c/f750/b859/slideshow/casa-2c-baquio-arquitectura_15.jpg?1697174657",
+      ],
+      url: "#",
+    },
+    {
+      id: 6,
+      ProjectName: "A-Frame House",
+      projectImages: [
+        "https://images.adsttc.com/media/images/6539/de01/08e4/4441/3ba1/519f/slideshow/a-frame-birdseye_2.jpg?1698291229",
+        "https://images.adsttc.com/media/images/6539/de02/08e4/4441/3ba1/51a1/slideshow/a-frame-birdseye_1.jpg?1698291237",
+        "https://images.adsttc.com/media/images/6539/ddfa/f96c/7601/7c71/9be1/slideshow/a-frame-birdseye_6.jpg?1698291231",
+      ],
+      url: "#",
+    },
+    {
+      id: 7,
+      ProjectName: "Ciasem House",
+      projectImages: [
+        "https://images.adsttc.com/media/images/650d/f687/67ee/e12e/9395/9de3/slideshow/ciasem-house-studiokas_1.jpg?1695413926",
+        "https://images.adsttc.com/media/images/650d/f689/67ee/e12e/9395/9de7/slideshow/ciasem-house-studiokas_2.jpg?1695413957",
+        "https://images.adsttc.com/media/images/650d/f68a/67ee/e12e/9395/9de9/slideshow/ciasem-house-studiokas_19.jpg?1695413977",
+      ],
+      url: "#",
+    },
+    {
+      id: 8,
+      ProjectName: "Piano House",
+      projectImages: [
+        "https://images.adsttc.com/media/images/5829/dc4f/e58e/cef9/f300/0019/slideshow/0043.jpg?1479138377",
+        "https://images.adsttc.com/media/images/5829/db05/e58e/cef9/f300/000d/slideshow/0020.jpg?1479138047",
+        "https://images.adsttc.com/media/images/5829/da66/e58e/cef9/f300/0005/slideshow/00046.jpg?1479137888",
+      ],
+      url: "#",
+    },
+    {
+      id: 9,
+      ProjectName: "Gea House",
+      projectImages: [
+        "https://images.adsttc.com/media/images/6528/aa76/64e5/0c4c/f750/b7e0/slideshow/casa-gea-studio-am11_5.jpg?1697163926",
+        "https://images.adsttc.com/media/images/6528/aa67/0e26/f34b/abc8/c082/slideshow/casa-gea-studio-am11_3.jpg?1697163903",
+        "https://images.adsttc.com/media/images/6528/aa67/64e5/0c4c/f750/b7dc/slideshow/casa-gea-studio-am11_1.jpg?1697163908",
+      ],
+      url: "#",
+    },
+  ],
+};
 
-const ProjectsListData = [
-  {
-    id: 1,
-    ProjectName: "WOHNDESIGN",
-    projectImage: [project1],
-    url: "#",
-  },
-  {
-    id: 1,
-    ProjectName: "EDMA GMBH",
-    projectImage: [project2],
-    url: "#",
-  },
-  {
-    id: 1,
-    ProjectName: "SPOT-TEC IMMOBILIEN AG",
-    projectImage: [project3],
-    url: "#",
-  },
-];
-
-const ServicesData = [
-  {
-    id: 1,
-    CompanyName: "ircon",
-    img: [logo1],
-    url: "http://www.ircon.cz/",
-  },
-];
-
-const ClientsData = [
-  {
-    id: 1,
-    CompanyName: "ircon",
-    img: [logo1],
-    url: "http://www.ircon.cz/",
-  },
-  {
-    id: 2,
-    CompanyName: "topolwater",
-    img: [logo2],
-    url: "http://old.topolwater.eu/english/index.html",
-  },
-  {
-    id: 3,
-    CompanyName: "czech republic",
-    img: [logo4],
-    url: "http://www.czechaid.cz/",
-  },
-  {
-    id: 4,
-    CompanyName: "republika e kosoves",
-    img: [logo3],
-    url: "https://ekosova.rks-gov.net/",
-  },
-];
-
-export { ClientsData, ServicesData, ProjectsListData };
+const ClientsData = {
+  clients: [
+    {
+      id: 1,
+      CompanyName: "ircon",
+      img: [logo1],
+      url: "http://www.ircon.cz/",
+    },
+    {
+      id: 2,
+      CompanyName: "topolwater",
+      img: [logo2],
+      url: "http://old.topolwater.eu/english/index.html",
+    },
+    {
+      id: 3,
+      CompanyName: "czech republic",
+      img: [logo4],
+      url: "http://www.czechaid.cz/",
+    },
+    {
+      id: 4,
+      CompanyName: "republika e kosoves",
+      img: [logo3],
+      url: "https://ekosova.rks-gov.net/",
+    },
+  ],
+};

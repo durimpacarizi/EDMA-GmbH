035ad36c445e7751cef2b51607f13491c6303645
durimpacarizi <150191578+durimpacarizi@users.noreply.github.com>
2023-12-06 08:08:34 +0100

    validimi 90% done

--- loginForm.js
+++ loginForm.js
@@ -1,53 +1,59 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  const submitBtn = document.getElementById("submit-btn");
+const userName = document.getElementById("userid");
+const passWord = document.getElementById("pass");
+const fullName = document.getElementById("emri");
+const email = document.getElementById("adresaEmailit");
+const submitBtn = document.getElementById("submit-btn");
 
+document.addEventListener("DOMContentLoaded", function (event) {
   const validate = (event) => {
     event.preventDefault();
-    const userName = document.getElementById("userid");
-    const passWord = document.getElementById("pass");
-    const fullName = document.getElementById("emri");
-    const email = document.getElementById("adresaEmailit");
+
+    console.log("submit eshte prekur");
 
     if (userName.value === "") {
       document.getElementById("h1").classList.remove("hide");
-      userName.focus();
+      submitBtn.disabled = true;
       submitBtn.classList.add("nope");
+      userName.classList.add("red");
+      userName.focus();
       return false;
     }
-    submitBtn.classList.remove("nope");
-    document.getElementById("h1").classList.add("hide");
 
-    if (passWord.value === "") {
+    if (
+      passWord.value.length < 8 &&
+      !(passWord.value.charAt(0) >= "A" && passWord.value.charAt(0) <= "Z") &&
+      !endsWithSpecialCharacter(passWord.value)
+    ) {
+      console.log("aaa");
       document.getElementById("h2").classList.remove("hide");
       passWord.focus();
+      submitBtn.disabled = true;
+      passWord.classList.add("red");
       submitBtn.classList.add("nope");
       return false;
     }
+
     submitBtn.classList.remove("nope");
     document.getElementById("h2").classList.add("hide");
 
-    if (fullName.value === "") {
+    if (!(fullName.value.length > 3 && fullName.value.includes(" "))) {
       document.getElementById("h3").classList.remove("hide");
       fullName.focus();
+      submitBtn.disabled = true;
+      fullName.classList.add("red");
       submitBtn.classList.add("nope");
       return false;
     }
     submitBtn.classList.remove("nope");
     document.getElementById("h3").classList.add("hide");
 
-    if (email.value === "") {
+    if (!(email.value.includes("@") && email.value.includes("."))) {
       document.getElementById("h4").classList.remove("hide");
       email.focus();
-      submitBtn.classList.add("nope");
-      return false;
-    }
-    submitBtn.classList.remove("nope");
-    document.getElementById("h4").classList.add("hide");
-    if (!(email.value.includes("@") && email.value.includes("."))) {
-      document.getElementById("h5").classList.remove("hide");
-      email.focus();
+      submitBtn.disabled = true;
+      email.classList.add("red");
       submitBtn.classList.add("nope");
       return false;
     }
@@ -56,3 +62,49 @@ document.addEventListener("DOMContentLoaded", function (event) {
   };
   submitBtn.addEventListener("click", validate);
 });
+
+userName.addEventListener("input", function () {
+  if (userName.value !== "") {
+    document.getElementById("h1").classList.add("hide");
+    submitBtn.disabled = false;
+    userName.classList.remove("red");
+    submitBtn.classList.remove("nope");
+  }
+});
+passWord.addEventListener("input", function () {
+  if (
+    passWord.value.length > 8 &&
+    passWord.value.charAt(0) >= "A" &&
+    passWord.value.charAt(0) <= "Z" &&
+    endsWithSpecialCharacter(passWord.value)
+  ) {
+    document.getElementById("h2").classList.add("hide");
+    submitBtn.disabled = false;
+    passWord.classList.remove("red");
+    submitBtn.classList.remove("nope");
+  }
+});
+
+fullName.addEventListener("input", function () {
+  if (fullName.value.length > 3 && fullName.value.includes(" ")) {
+    document.getElementById("h3").classList.add("hide");
+    submitBtn.disabled = false;
+    fullName.classList.remove("red");
+    submitBtn.classList.remove("nope");
+  }
+});
+
+email.addEventListener("input", function () {
+  if (email.value.includes("@") && email.value.includes(".")) {
+    document.getElementById("h4").classList.add("hide");
+    submitBtn.disabled = false;
+    email.classList.remove("red");
+    submitBtn.classList.remove("nope");
+  }
+});
+
+function endsWithSpecialCharacter(str) {
+  const specialChars = /[0123456789!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+  return specialChars.test(str.charAt(str.length - 1));
+}
